48b3dfa1e4fdfde2f26b589c1f343b2e64d0b2a1
ehdrjs4502 <ehdrjs4502@naver.com>
2024-02-10 23:15:06 +0900

    Next.js 14 공부 (Hydration)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h2>ㅎㅇ</h2>
+      <button onClick={() => setCount((c) => c + 1)}>{count}</button>
     </div>
   );
 }
