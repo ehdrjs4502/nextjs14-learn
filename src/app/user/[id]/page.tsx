303da5f59d2451b757ae8150e253f70f83d39966
ehdrjs4502 <ehdrjs4502@naver.com>
2024-02-18 17:03:42 +0900

    영화 찜 목록 구현

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -4,7 +4,14 @@ import style from "../../styles/user/page.module.css";
 
 export default async function User({ params }: any) {
   const movies = await getFavoriteMovies(params.id);
-  console.log(movies);
+  console.log("찜 영화 : ", movies);
+
+  if (movies.message === "Failed to fetch favorites")
+    return (
+      <div>
+        <h4>올바르지 않은 사용자입니다.</h4>
+      </div>
+    );
   return (
     <div className={style.container}>
       <h4>찜한 영화 목록</h4>
