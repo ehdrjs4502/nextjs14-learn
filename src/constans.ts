e98e350c9ed993a4c103f7de8aaa6036ee859512
ehdrjs4502 <ehdrjs4502@naver.com>
2024-02-24 20:10:34 +0900

    로컬 서버로 변경

--- src/constans.ts
+++ src/constans.ts
@@ -1,2 +1,3 @@
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
-export const SERVER_URL = "https://port-0-nextjs14-movie-back-dc9c2nlssm6huq.sel5.cloudtype.app";
+export const SERVER_URL = "http://localhost:3001";
+// "https://port-0-nextjs14-movie-back-dc9c2nlssm6huq.sel5.cloudtype.app"
