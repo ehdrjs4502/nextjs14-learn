e8204b90feac0dca2ab446396b05c16a9f0e9aa5
ehdrjs4502 <ehdrjs4502@naver.com>
2024-03-01 23:37:40 +0900

    로그인 페이지 style 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ import useUserInfo from "@/_hooks/useUserInfo";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import style from "../_styles/login.module.css";
 
 export default function Login() {
   const router = useRouter();
@@ -26,35 +27,44 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <h4>Login</h4>
-      <span>test 계정으로 로그인하실 수 있습니다.</span>
-      <form>
-        <div>
-          <label htmlFor="id">ID:</label>
-          <input type="text" id="id" name="id" value={id} onChange={(e) => setId(e.target.value)} placeholder="test" />
-        </div>
-        <div>
-          <label htmlFor="pw">Password:</label>
-          <input
-            type="password"
-            id="pw"
-            name="pw"
-            value={pw}
-            onChange={(e) => setPw(e.target.value)}
-            placeholder="1234"
-          />
-        </div>
-        <button
-          type="button"
-          onClick={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          Login
-        </button>
-        {isInvalid && <span>유효하지 않은 ID입니다.</span>}
-      </form>
+    <div className={style.container}>
+      <div className={style.box}>
+        <h4>Login</h4>
+        <span>test 계정으로 로그인하실 수 있습니다.</span>
+        <form>
+          <div>
+            <input
+              type="text"
+              id="id"
+              name="id"
+              autoFocus
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              placeholder="test"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="pw"
+              name="pw"
+              value={pw}
+              onChange={(e) => setPw(e.target.value)}
+              placeholder="1234"
+            />
+          </div>
+          {isInvalid && <b>유효하지 않은 ID입니다.</b>}
+          <button
+            type="button"
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+            className={style.btn}
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
